ad5f35fb828a5fa6795bb930790a714c60137a99
rahobi <54870203+rahobi@users.noreply.github.com>
2019-09-16 16:36:58 -0500

    Add files via upload

---
+++ index.hyperesources/triviaInfo.js
@@ -0,0 +1,78 @@
+function getInfo() {
+	window.numberOfQuestions = 	10;
+	window.currentSlide = 		0;
+	window.currentScore = 		0;
+	window.title = 				"GOLF TRIVIA";
+	window.questions = [		"What year was the PGA of America founded?", 
+								"Who was named PGA Tour Rookie of the Year in 1996?",
+								"Who was the first winner of the U.S. Women's Open?", 
+								"Who was the first golfer to reach one million dollars in career earnings on the PGA Tour?", 
+								"What golfer was known as The Golden Bear?",
+								"In 2004, who became the oldest golfer to win PGA Tour Rookie of the Year?",
+								"What type of golf clubs are used for long shots from the tee or fairway?",
+								"What golfer invented the modern sand wedge?",
+								"What golfer set a PGA Tour record in 1945 by winning 18 tournaments?",
+								"What golfer replaced Tiger Woods at the top of the Official World Golf Rankings in 2004?" ];
+	window.correctAnswer = [ 	"B", "C", "A", "A", "B", "B", "A", "C", "D", "C" ]; 
+	window.answerA = [ 			"1875", 
+								"Phil Mickelson",
+								"Patty Berg",
+								"Arnold Palmer",
+								"Tiger Woods",
+								"Tiger Woods",
+								"Woods",
+								"Happy Gilmore",
+								"Tiger Woods",
+								"Tiger Woods" ];
+	window.answerB = [ 			"1916",
+								"Matt Kuchar",
+								"Babe Zaharias",
+								"Jack Nicklaus",
+								"Todd Hamilton",
+								"Irons",
+								"Ben Hogan",
+								"Arnold Palmer",
+								"Arnold Palmer" ];
+	window.answerC = [ 			"1928",
+								"Tiger Woods",
+								"Louise Suggs",
+								"Bobby Jones",
+								"Ben Hogan",
+								"Ben Hogan",
+								"Wedges",
+								"Gene Sarazen",
+								"Ben Hogan",
+								"Vijay Singh" ];
+	window.answerD = [ 			"1975",
+								"Arnold Palmer",
+								"Judy Rankin",
+								"Tiger Woods",
+								"Arnold Palmer",
+								"Arnold Palmer",
+								"Putter",
+								"Tiger Woods",
+								"Byron Nelson",
+								"Gene Sarazen" ];
+	window.answerE = [ 			"2002",
+								"Jim Furyk",
+								"Claire Page",
+								"Phil Mickelson",
+								"Justin Thomas",
+								"Rory Mcllroy",
+								"Hybrid",
+								"Justin Thomas",
+								"Ian Poulter",
+								"Rickie Fowler" ];												
+	window.response = [ 		"The PGA (Professional Golfer's Association) of America, a not-for-profit organization that promotes the game of golf, was founded on April 10, 1916.", 
+								"Tiger Woods turned professional in September of 1996. Competing in the final five regular PGA TOUR tournaments of the year, he finished 5th, 3rd, 1st, 3rd, and 1st, placing him among the top 30 money-winners.'", 
+								"In 1946, Patty Berg won the very first U.S. Women's Open.", 
+								"In 1967, Arnold Palmer became the first golfer to reach one million dollars in career earnings on the PGA Tour.", 
+								"Jack Nicklaus, considered by many critics to be the greatest golfer of all time, was known as The Golden Bear." ,
+								"In 2004, at the age of 39, Todd Hamilton became the oldest golfer to win PGA Tour Rookie of the Year." ,
+								"Woods are used for long shots from the tee or fairway." ,
+								"Gene Sarazen invented the modern sand wedge in 1930." ,
+								"Byron Nelson set a PGA Tour record in 1945 by winning 18 tournaments -- including 11 tournaments in a row!" ,
+								"On September 6, 2004, Vijay Singh replaced Tiger Woods at the top of the Official World Golf Rankings, ending Woods' reign of more than five years" ];
+							
+}
+						
